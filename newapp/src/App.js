ec7b218caa0b6fd9fcbb86f87a0b5b0b268578c2
infokhairul <infokhairul99@gmail.com>
2021-01-12 11:08:36 -0800

    Fast Commit

---
+++ newapp/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Hello from './Componets/Hello';
+import NavBar from './Componets/NavBar';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Hello></Hello>
+     <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ newapp/src/Componets/Hello.js
@@ -0,0 +1,15 @@
+import Reacts from 'react';
+import '../Componets/HeaderTop.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function Hello(){
+
+    return(
+        <div>
+
+        </div>
+    )
+
+} export default Hello;
